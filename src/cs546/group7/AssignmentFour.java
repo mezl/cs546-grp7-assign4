15e9a97cd0efc7ccbf0608ea7c3df52683ef0e8a
mviswana@usc.edu <mviswana@usc.edu@ed8e9b04-0e91-11de-ae1d-37bcf2f1229e>
2009-03-11 23:23:28 +0000

    Auto-generated by Android's activitycreator script

---
+++ src/cs546/group7/AssignmentFour.java
@@ -0,0 +1,15 @@
+package cs546.group7;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AssignmentFour extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
