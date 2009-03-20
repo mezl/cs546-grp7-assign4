265a91a0ee4f75910502fc43cdf21f3221a55f1e
mviswana@usc.edu <mviswana@usc.edu@ed8e9b04-0e91-11de-ae1d-37bcf2f1229e>
2009-03-20 04:26:42 +0000

    Preliminary code for the main UI

--- trunk/src/cs546/group7/AssignmentFour.java
+++ trunk/src/cs546/group7/AssignmentFour.java
@@ -1,15 +1,119 @@
-package cs546.group7;
+/*
+   *******************************************************************
+   *                                                                 *
+   *    CSCI-546, Spring 2009 Assignment IV Solution by Group #7     *
+   *                                                                 *
+   *    Group Members: Chang, Chin-Kai      chinkaic@usc.edu         *
+   *                   Moon, Ji Hyun        jihyunmo@usc.edu         *
+   *                   Patlolla, Avinash    patlolla@usc.edu         *
+   *                   Viswanathan, Manu    mviswana@usc.edu         *
+   *                                                                 *
+   *******************************************************************
+   *                                                                 *
+   * This file contains the main code for a turn-by-turn walking     *
+   * directions application. This application is meant to be used on *
+   * the USC University Park Campus.
+   *                                                                 *
+   *******************************************************************
+*/
 
-import android.app.Activity;
-import android.os.Bundle;
+/*
+   AssignmentFour -- turn-by-turn walking directions for USC campus on gPhone
 
-public class AssignmentFour extends Activity
+   Copyright (C) 2009 Chin-Kai Chang
+                      Ji Hyun Moon
+                      Avinash Patlolla
+                      Manu Viswanathan
+
+   This file is part of AssignmentFour.
+
+   AssignmentFour is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   AssignmentFour is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with AssignmentFour; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA.
+*/
+
+/*
+  REVISION HISTORY
+
+  $HeadURL$
+  $Id$
+*/
+
+//----------------------- PACKAGE SPECIFICATION -------------------------
+
+package cs546.group7 ;
+
+//------------------------------ IMPORTS --------------------------------
+
+// Android UI support
+import android.widget.TextView ;
+
+// Android application and OS support
+import android.app.Activity ;
+import android.os.Bundle ;
+
+//--------------------- APPLICATION'S MAIN SCREEN -----------------------
+
+/**
+   This class implements the main screen of the turn-by-turn walking
+   directions application. The main screen consists of a "status" bar on
+   top that shows the current location and a search textbox at the bottom
+   for users to type in destination building codes. In the middle is a
+   multitab area with two tabs: one for the walking directions and
+   another displaying the campus map.
+*/
+public class AssignmentFour extends Activity {
+
+//-------------------------- INITIALIZATION -----------------------------
+
+/// This method is called when the activity is first created. It is akin
+/// to the "main" function in normal desktop applications.
+@Override public void onCreate(Bundle saved_state)
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+   super.onCreate(saved_state) ;
+   setContentView(R.layout.main) ;
+
+   // Setup the GPS listener
+   GPSRecorder G = GPSRecorder.create(this) ;
+   G.use_ui((TextView) findViewById(R.id.lat_view),
+            (TextView) findViewById(R.id.lon_view)) ;
 }
+
+/// Called when the activity is resumed. In our case, we reconnect to the
+/// audio tags database.
+@Override protected void onResume()
+{
+   super.onResume() ;
+}
+
+//----------------------------- CLEAN-UP --------------------------------
+
+/// Called when the activity ends. In our app, we should close the
+/// connection to the database.
+@Override protected void onPause()
+{
+   super.onPause() ;
+}
+
+/// On final application close, we have to shutdown the GPS listener that
+/// we created during application start-up.
+@Override protected void onDestroy()
+{
+   GPSRecorder.instance(this).shutdown() ;
+   super.onDestroy() ;
+}
+
+//-----------------------------------------------------------------------
+
+} // end of class cs546.group7.AssignmentFour
---
+++ trunk/src/cs546/group7/Utils.java
@@ -0,0 +1,167 @@
+/*
+   *******************************************************************
+   *                                                                 *
+   *    CSCI-546, Spring 2009 Assignment IV Solution by Group #7     *
+   *                                                                 *
+   *    Group Members: Chang, Chin-Kai      chinkaic@usc.edu         *
+   *                   Moon, Ji Hyun        jihyunmo@usc.edu         *
+   *                   Patlolla, Avinash    patlolla@usc.edu         *
+   *                   Viswanathan, Manu    mviswana@usc.edu         *
+   *                                                                 *
+   *******************************************************************
+   *                                                                 *
+   * This file contains an assortment of utility functions used by   *
+   * different parts of the walking directions application.          *
+   *                                                                 *
+   *******************************************************************
+*/
+
+/*
+   AssignmentFour -- a photo manager application for the Google Phone
+
+   Copyright (C) 2009 Chin-Kai Chang
+                      Ji Hyun Moon
+                      Avinash Patlolla
+                      Manu Viswanathan
+
+   This file is part of AssignmentFour.
+
+   AssignmentFour is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   AssignmentFour is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with AssignmentFour; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA.
+*/
+
+/*
+  REVISION HISTORY
+
+  $HeadURL$
+  $Id$
+*/
+
+//----------------------- PACKAGE SPECIFICATION -------------------------
+
+package cs546.group7 ;
+
+//------------------------------ IMPORTS --------------------------------
+
+// Android UI support
+import android.widget.Toast ;
+import android.app.AlertDialog ;
+
+// Android application and OS support
+import android.content.Context ;
+
+// Android utilities
+import android.util.Log ;
+
+// Java I/O support
+import java.io.FileInputStream ;
+import java.io.InputStream ;
+import java.io.OutputStream ;
+import java.io.File ;
+import java.io.FilenameFilter ;
+
+//------------------------- CLASS DEFINITION ----------------------------
+
+/**
+   This class provides several handy utility functions.
+*/
+class Utils {
+
+//-------------------------- GPS COORDINATES ----------------------------
+
+/// A simple pair to hold latitude and longitude
+public static final class LatLong {
+   public double latitude ;
+   public double longitude ;
+
+   public LatLong(double latitude, double longitude) {
+      this.latitude  = latitude ;
+      this.longitude = longitude ;
+   }
+}
+
+//------------------------- UI NOTIFICATIONS ----------------------------
+
+/// A short notification message that doesn't steal focus or require any
+/// specific interaction on the user's part to dismiss. It simply appears
+/// briefly and fades away.
+public final static void notify(Context C, String msg)
+{
+   Toast.makeText(C, msg, Toast.LENGTH_SHORT).show() ;
+}
+
+/// Long notification message
+public final static void notify_long(Context C, String msg)
+{
+   Toast.makeText(C, msg, Toast.LENGTH_LONG).show() ;
+}
+
+/// Show an error box
+public final static void alert(Context C, String msg)
+{
+   AlertDialog.Builder alert = new AlertDialog.Builder(C) ;
+   alert.setMessage(msg) ;
+   alert.setPositiveButton(R.string.alert_okay_label, null) ;
+   alert.show() ;
+}
+
+//----------------------- FILE SYSTEM FUNCTIONS -------------------------
+
+/// This function returns true if the specified file exists, is readable
+/// and actually has some data in it; false otherwise.
+public final static boolean exists(String file_name)
+{
+   File f = new File(file_name) ;
+   return f.exists() && f.canRead() && f.length() > 0 ;
+}
+
+/// This function removes the specified file
+public final static void unlink(String file_name)
+{
+   new File(file_name).delete() ;
+}
+
+/// Remove all files matching specified pattern
+public final static void unlink_all(final String glob)
+{
+   final int last_sep = glob.lastIndexOf(File.separatorChar) ;
+
+   File dir = new File(glob.substring(0, last_sep)) ;
+   String[] list = dir.list(new FilenameFilter() {
+         private String pattern = new String(glob.substring(last_sep + 1));
+         public  boolean accept(File dir, String file_name) {
+            return file_name.matches(pattern) ;
+         }
+      }) ;
+
+   for (int i = 0; i < list.length; ++i)
+      unlink(dir.getPath() + File.separator + list[i]) ;
+}
+
+/// Copy the named file byte-by-byte to the supplied output stream
+public final static
+void copy(String file_name, OutputStream out) throws Exception
+{
+   InputStream in = new FileInputStream(file_name) ;
+   byte[] buf = new byte[512] ;
+   int len ;
+   while ((len = in.read(buf)) > 0)
+      out.write(buf, 0, len) ;
+   in.close() ;
+}
+
+//-----------------------------------------------------------------------
+
+} // end of class cs546.group7.Utils
