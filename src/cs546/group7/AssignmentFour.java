c760551cb2909eb6f6e7b0428bd8e2d793f76ce5
mviswana@usc.edu <mviswana@usc.edu@ed8e9b04-0e91-11de-ae1d-37bcf2f1229e>
2009-03-24 05:04:11 +0000

    1. Fixed a critical bug in the GPS recorder that was causing it to    shutdown prematurely. Apparently, Android calls onDestroy() even when    it isn't necessarily going to really kill the application? Anyhoo, the    problem in the GPS recorder was still going to remain because of its    singleton nature and other general weirdness...
    2. Did a little clean-up of the directions related code and moved some of
       the helper functions into the utils module.
    
    3. Minor change to the UI.

--- src/cs546/group7/AssignmentFour.java
+++ src/cs546/group7/AssignmentFour.java
@@ -111,6 +111,29 @@ public class AssignmentFour extends Activity {
    super.onResume() ;
 }
 
+//------------------------ WALKING DIRECTIONS ---------------------------
+
+private Problems p;
+
+private void show_direction(String[] direction)
+{
+  // direction_list.setAdapter(
+  //    new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,direction));
+}
+
+public void init_map(String map)
+{
+   p = new Problems(Utils.fullname(this, map)) ;
+}
+
+public void search(String from, String to)
+{
+   p.setInitial_state(from);
+   p.setGoal_state(to);
+   p.Search(Registry.SEARCH_UNI_COST, null);
+   show_direction(p.getDirection());
+}
+
 //----------------------------- CLEAN-UP --------------------------------
 
 /// Called when the activity ends. In our app, we should close the
@@ -129,34 +152,5 @@ public class AssignmentFour extends Activity {
 }
 
 //-----------------------------------------------------------------------
-
-private Problems p;
-private void show_direction(String[] direction)
-{  
-  // direction_list.setAdapter(
-  //    new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,direction));
-}
-public void init_map(String map) {
-	p = new Problems(fullname(map));// "data/data/android.four/files/map.txt"
-}
-public void search(String from, String to) {
-	p.setInitial_state(from);
-	p.setGoal_state(to);
-	p.Search(Registry.SEARCH_UNI_COST, null);
-	show_direction(p.getDirection());
-}
-
-// / Quick helper to extract just the file name from a given path name (in
-// / case the user entered a full path, which Android does not allow).
-private String basename(String path_name) {
-	return new File(path_name).getName();
-}
-
-// / Although Android does not allow full path names, we still need those
-// / to check for file existence, etc.
-private String fullname(String file_name) {
-	return getFilesDir().getPath() + File.separator + basename(file_name);
-}
-
 
 } // end of class cs546.group7.AssignmentFour
--- src/cs546/group7/GPSRecorder.java
+++ src/cs546/group7/GPSRecorder.java
@@ -69,10 +69,13 @@ import android.os.Bundle ;
 // Android utilities
 import android.util.Log ;
 
+// Java containers
+import java.util.ArrayList ;
+import java.util.List ;
+
 // Java utilities
 import java.text.DecimalFormat ;
 import java.util.Date ;
-import java.util.List ;
 
 //------------------------- CLASS DEFINITION ----------------------------
 
@@ -115,6 +118,27 @@ private Location m_location ;
 private TextView m_lat_view ;
 private TextView m_lon_view ;
 
+//----------------------------- CALLBACK --------------------------------
+
+/// This interface allows clients to receive GPS update notifications.
+/// Any object that wants these notifications must implement this
+/// interface and register the callback object with the GPS recorder.
+public interface RefreshCB {
+   public abstract void gpsUpdated(Location L) ;
+}
+
+/// The GPS recorder maintains a list of callback objects that will be
+/// triggered when its GPS listener sends it an update notification.
+private List<RefreshCB> m_callbacks ;
+
+/// This method allows clients of the GPS recorder to add callback
+/// objects that should be triggered whenever new coordinates become
+/// available.
+public void addCallback(RefreshCB cb)
+{
+   m_callbacks.add(cb) ;
+}
+
 //-------------------------- INITIALIZATION -----------------------------
 
 /// Private constructor because this is a singleton
@@ -122,6 +146,8 @@ private GPSRecorder(Context C)
 {
    m_context = C ;
    setup_listener(m_context) ;
+   m_callbacks = new ArrayList<RefreshCB>() ;
+   show_location() ;
 }
 
 /// The singleton object's lone instance
@@ -172,27 +198,6 @@ private void setup_listener(Context C)
       shutdown() ;
       m_location = null ;
    }
-}
-
-//----------------------------- CALLBACK --------------------------------
-
-/// This interface allows clients to receive GPS update notifications.
-/// Any object that wants these notifications must implement this
-/// interface and register the callback object with the GPS recorder.
-public interface RefreshCB {
-   public abstract void gpsUpdated(Location L) ;
-}
-
-/// The GPS recorder maintains a list of callback objects that will be
-/// triggered when its GPS listener sends it an update notification.
-private List<RefreshCB> m_callbacks ;
-
-/// This method allows clients of the GPS recorder to add callback
-/// objects that should be triggered whenever new coordinates become
-/// available.
-public void addCallback(RefreshCB cb)
-{
-   m_callbacks.add(cb) ;
 }
 
 //--------------------------- THE LISTENER ------------------------------
@@ -253,6 +258,7 @@ public void shutdown()
       (LocationManager) m_context.getSystemService(Context.LOCATION_SERVICE) ;
    M.removeUpdates(m_listener) ;
    m_listener = null ;
+   m_instance = null ;
 }
 
 //------------------------------ HELPERS --------------------------------
